2478fa4856598bd75c80f350ed2553fe49f79ec0
JacobLinCool <jacoblincool@gmail.com>
2022-10-17 18:47:47 +0800

    feat: optional year/ term, fallback to current ones

--- src/api/course/index.ts
+++ src/api/course/index.ts
@@ -3,30 +3,43 @@ import { z } from "zod";
 import debug from "debug";
 import { prisma } from "@/prisma";
 import { Err, Ok } from "@/response";
+import { date2term } from "@/utils";
 
 const log = debug("api:course");
 const router = new Router();
 
 const query = z.object({
-    year: z.string().transform(year => parseInt(year)).refine(year => year >= 107 && year <= 111),
-    term: z.string().transform(term => parseInt(term)).refine(term => term >= 1 && term <= 2),
     q: z.string().min(1)
 });
 
 router.get("/", async ctx => {
     try {
-        const { year, term, q } = query.parse(ctx.query);
+        const { q } = query.parse(ctx.query);
+        const { year, term, key } = parse_query(q);
 
-        log("Querying courses", year, term, q);
+        log("Querying courses", year, term, key);
+
         const courses = await prisma.course.findMany({
             where: {
                 year,
                 term,
                 OR: [
-                    { teachers: { some: { name: q } } },
-                    { programs: { some: { name: { contains: q } } } },
-                    { name: { contains: q } },
-                    { code: q }
+                    {
+                        teachers: {
+                            some: {
+                                OR: key.map(k => ({ name: { contains: k } }))
+                            }
+                        }
+                    },
+                    {
+                        programs: {
+                            some: {
+                                OR: key.map(k => ({ name: { contains: k.replace("學程", "") } }))
+                            }
+                        }
+                    },
+                    { OR: key.map(k => ({ name: { contains: k } })) },
+                    { code: { in: key } }
                 ]
             },
             include: {
@@ -48,3 +61,34 @@ router.get("/", async ctx => {
 });
 
 export default router;
+
+function parse_query(q: string): { school: string; year: number; term: number; key: string[] } {
+    const regexp = /(?:"[^"]*"|[^"\s]+)/g;
+    const matches = q.match(regexp);
+
+    if (matches === null) {
+        return { school: "", year: 0, term: 0, key: [] };
+    }
+
+    let school = "";
+    let [year, term] = date2term();
+    const key: string[] = [];
+
+    for (let match of matches) {
+        if (match[0] === "\"" && match[match.length - 1] === "\"") {
+            match = match.slice(1, -1);
+        }
+
+        if (match.startsWith("school:")) {
+            school = match.slice(7);
+        } else if (match.startsWith("year:")) {
+            year = parseInt(match.slice(5));
+        } else if (match.startsWith("term:")) {
+            term = parseInt(match.slice(5));
+        } else {
+            key.push(match);
+        }
+    }
+
+    return { school, year, term, key };
+}
