ef4ee055338816b6bbbf6ad1df941b5637582c29
JacobLinCool <jacoblincool@gmail.com>
2022-11-08 01:43:32 +0800

    feat(core): me endpoint

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import courses from "@/api/courses";
 import auth from "@/api/auth";
 import profile from "@/api/profile";
 import manage from "@/api/manage";
+import me from "@/api/me";
 
 const router = new UniRouter()
     .use("/health", health.routes())
@@ -15,7 +16,8 @@ const router = new UniRouter()
     .use("/auth", auth.routes())
     .use("/courses", create_guard(["verified"]), courses.routes())
     .use("/profile", create_guard(["verified"]), profile.routes())
-    .use("/manage", create_guard(["moderator", "verified"]), manage.routes());
+    .use("/manage", create_guard(["moderator", "verified"]), manage.routes())
+    .use("/me", create_guard(), me.routes());
 
 const server = new Koa()
     .use(catcher)
