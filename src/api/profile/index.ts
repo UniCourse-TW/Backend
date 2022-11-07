e3ae7207c6580f2552db89cd4c35b3dd698b0be1
JacobLinCool <jacoblincool@gmail.com>
2022-11-08 01:43:51 +0800

    feat(core): profile patch update

--- src/api/profile/index.ts
+++ src/api/profile/index.ts
@@ -40,4 +40,60 @@ router.get("/:username", async ctx => {
     }
 });
 
+router.patch("/:username", async ctx => {
+    if (!ctx.state.token) {
+        ctx.err("Not logged in", { code: 401 });
+        return;
+    }
+
+    try {
+        const { username } = ctx.params;
+
+        if (username !== ctx.state.token.username && !ctx.state.token.traits.includes("admin")) {
+            ctx.err("Not authorized", { code: 403 });
+            return;
+        }
+
+        const schema = z.object({
+            name: z.string().min(1).max(64).optional(),
+            bio: z.string().max(1024).optional(),
+            school: z.string().max(1024).optional(),
+            email: z.string().email().max(1024).optional(),
+            location: z.string().max(1024).optional(),
+            banner: z.string().url().max(1024).optional(),
+            avatar: z.string().url().max(1024).optional(),
+            extra: z.object({}).optional()
+        }).strict();
+
+        const data = schema.parse(ctx.request.body);
+
+        log("updating profile for %s", username);
+        const last = await prisma.userSnapshot.findFirst({
+            where: { username, revoked: false },
+            orderBy: { id: "desc" }
+        });
+
+        if (!last) {
+            ctx.err("Data not found", { code: 404 });
+            return;
+        }
+
+        const profile = await prisma.userProfile.update({
+            where: { user_id: last.user_id },
+            data: {
+                ...data,
+                extra: {}
+            }
+        });
+
+        ctx.ok(profile);
+    } catch (err) {
+        if (err instanceof z.ZodError) {
+            ctx.err(err.message, { code: 400 });
+        } else {
+            throw err;
+        }
+    }
+});
+
 export default router;
