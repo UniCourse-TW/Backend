1d57493766737db263e59ef5e977a75787195597
JacobLinCool <jacoblincool@gmail.com>
2022-11-10 17:11:15 +0800

    feat(cli): register command

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,6 +2,7 @@
 import { program } from "commander";
 import whoami from "./commands/whoami";
 import login from "./commands/login";
+import register from "./commands/register";
 import logout from "./commands/logout";
 import status from "./commands/status";
 import req from "./commands/req";
@@ -10,6 +11,7 @@ import import_file from "./commands/import";
 program
     .addCommand(whoami)
     .addCommand(login)
+    .addCommand(register)
     .addCommand(logout)
     .addCommand(status)
     .addCommand(req)
