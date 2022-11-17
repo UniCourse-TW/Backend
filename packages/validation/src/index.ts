cd2dd57f062e604c018321a04519dd97eb3de49d
JacobLinCool <jacoblincool@gmail.com>
2022-11-17 21:27:32 +0800

    feat(validation): expose configuration

--- packages/validation/src/index.ts
+++ packages/validation/src/index.ts
@@ -1,13 +1,16 @@
+import * as config from "./config";
 import * as auth from "./auth";
 import * as search from "./search";
 import * as post from "./post";
 import * as safe from "./safe";
 
+export * from "./config";
 export * from "./auth";
 export * from "./search";
 export * from "./post";
 export * from "./safe";
 export const v = {
+    ...config,
     ...auth,
     ...search,
     ...post,
