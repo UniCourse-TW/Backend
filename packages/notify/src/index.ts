9747d0c009c75d7afd32ee3c54f403bdb67e191f
JacobLinCool <jacoblincool@gmail.com>
2022-11-18 14:11:09 +0800

    style(notify): format with ESLint

--- packages/notify/src/index.ts
+++ packages/notify/src/index.ts
@@ -1,13 +1,14 @@
-import * as nodemailer from 'nodemailer';
-import process from 'process';
+import * as nodemailer from "nodemailer";
+import type SMTPTransport from "nodemailer/lib/smtp-transport";
 
 // Create email transporter from config (env var)
 function createTransporter() {
     const user = process.env.SMTP_USER;
     const pass = process.env.SMTP_PASS;
 
-    if(user === undefined || pass == undefined)
+    if (user === undefined || pass === undefined) {
         throw new Error("No SMTP config");
+    }
 
     return nodemailer.createTransport({
         host: "mail.gandi.net",
@@ -15,18 +16,19 @@ function createTransporter() {
         port: 465,
         auth: {
             user,
-            pass,
+            pass
         }
     });
 }
 
-export async function sendMail(to: string, subject: string, body: string)
-{
-    let transporter = createTransporter();
+export async function sendMail(
+    to: string, subject: string, body: string
+): Promise<SMTPTransport.SentMessageInfo> {
+    const transporter = createTransporter();
     return transporter.sendMail({
-        from: 'noreply@unicourse.tw',
+        from: "noreply@unicourse.tw",
         to,
         subject,
-        html: body,
+        html: body
     });
 }
