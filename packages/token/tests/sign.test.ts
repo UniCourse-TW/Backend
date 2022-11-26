3d8fe89603e14098c7d1edeb5c369603981b1646
JacobLinCool <jacoblincool@gmail.com>
2022-11-27 03:04:33 +0800

    test(token): add test for verify

--- packages/token/tests/sign.test.ts
+++ packages/token/tests/sign.test.ts
@@ -1,4 +1,4 @@
-import { decode, sign } from "../src";
+import { decode, sign, verify } from "../src";
 
 describe("sign", () => {
     test("accepted", () => {
@@ -26,5 +26,40 @@ describe("sign", () => {
         };
 
         expect(() => sign(token)).toThrow();
+    });
+
+    test("verify", () => {
+        const token = {
+            token: "cuid",
+            user: "cuid-user",
+            username: "username",
+            expires: Math.floor(Date.now() / 1_000) + 60,
+            traits: ["trait"]
+        };
+
+        const signed = sign(token);
+        const decoded = verify(signed);
+        expect(decoded).toEqual(token);
+    });
+
+    test("verify invalid", () => {
+        const token = {
+            token: "cuid",
+            user: "cuid-user",
+            username: "username",
+            expires: Math.floor(Date.now() / 1_000) + 60,
+            traits: ["trait"]
+        };
+
+        const signed = sign(token);
+
+        const parts = signed.split(".");
+        const payload = JSON.parse(Buffer.from(parts[1], "base64").toString());
+        payload.traits = ["invalid"];
+        const modified = [
+            parts[0], Buffer.from(JSON.stringify(payload)).toString("base64"), parts[2]
+        ].join(".");
+
+        expect(() => verify(modified)).toThrow();
     });
 });
